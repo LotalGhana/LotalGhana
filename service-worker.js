bb916153879f76568b2b4d2ab460c650b778b86b
LotalGhana <114388126+LotalGhana@users.noreply.github.com>
2025-06-28 17:35:25 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,3 +10,14 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(fetch(event.request));
 });
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    .then((reg) => {
+      console.log("Service Worker registered:", reg);
+    })
+    .catch((err) => {
+      console.error("Service Worker registration failed:", err);
+    });
+}
+
